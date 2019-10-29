07f5d51cf8d9a5b2a8373cacaf4ebee848675946
Josh Gummersall <josh@standardlabs.dev>
2019-10-29 16:47:13 -0700

    Support transforms

--- src/index.js
+++ src/index.js
@@ -1,13 +1,23 @@
-import React, {useCallback, useState} from 'react'
+import React, { useCallback, useState } from "react";
 
-export const useInput = (initialState) => {
-  const [value, setValue] = useState(initialState)
-  const onChange = useCallback(({target: {value}}) => setValue(value), [setValue])
-  return [value, onChange, setValue]
-}
+const identity = a => a;
 
-export const useCheckbox = (initialState) => {
-  const [checked, setChecked] = useState(initialState)
-  const onChange = useCallback(({target: {checked}}) => setChecked(checked), [setChecked])
-  return [checked, onChange, setChecked]
-}
+export const useInput = (initialState, transform = identity) => {
+  const [value, setValue] = useState(initialState);
+  const onChange = useCallback(
+    ({ target: { value } }) => setValue(transform(value)),
+    [setValue]
+  );
+
+  return [value, onChange, setValue];
+};
+
+export const useCheckbox = initialState => {
+  const [checked, setChecked] = useState(initialState);
+  const onChange = useCallback(
+    ({ target: { checked } }) => setChecked(checked),
+    [setChecked]
+  );
+
+  return [checked, onChange, setChecked];
+};
