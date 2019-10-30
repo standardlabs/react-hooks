93319fba0d9a2f3d796e34c3c59ee8070d1f2331
Josh Gummersall <josh@standardlabs.dev>
2019-10-29 18:40:59 -0700

    Deferred logic

--- src/index.js
+++ src/index.js
@@ -1,23 +1,62 @@
-import React, { useCallback, useState } from "react";
+import debounce from "lodash.debounce";
+import { useCallback, useState } from "react";
 
 const identity = a => a;
 
-export const useOnChange = (initialState, transform = identity) => {
+export const useDebounced = (initialState, wait) => {
+  const [state, setState] = useState(initialState);
+  const setStateDebounced = useCallback(debounce(setState, wait), [setState]);
+  return [state, setStateDebounced];
+};
+
+export const useDeferred = (initialState, wait) => {
   const [value, setValue] = useState(initialState);
+  const [deferred, setDeferred] = useDebounced(initialState, wait);
+
+  const setState = useCallback(
+    (...args) => {
+      setDeferred(...args);
+      return setValue(...args);
+    },
+    [setValue, setDeferred]
+  );
+
+  return [{ value, deferred }, setState];
+};
+
+const buildOnHandler = (useFn, key, argFn) => (...args) => {
+  const { initialState, wait, transform = identity } = argFn(args);
+
+  const [value, setValue] = useFn(initialState, wait);
   const onChange = useCallback(
-    ({ target: { value } }) => setValue(transform(value)),
+    ({ target }) => setValue(transform(target[key])),
     [setValue]
   );
 
   return [value, onChange, setValue];
 };
 
-export const useOnChecked = initialState => {
-  const [checked, setChecked] = useState(initialState);
-  const onChecked = useCallback(
-    ({ target: { checked } }) => setChecked(checked),
-    [setChecked]
-  );
+const useStateArgs = args => ({
+  initialState: args[0],
+  transform: args[1]
+});
 
-  return [checked, onChecked, setChecked];
-};
+const useDeferredArgs = args => ({
+  initialState: args[0],
+  wait: args[1],
+  transform: args[2]
+});
+
+export const useOnChange = buildOnHandler(useState, "value", useStateArgs);
+export const useDeferredOnChange = buildOnHandler(
+  useDeferred,
+  "value",
+  useDeferredArgs
+);
+
+export const useOnChecked = buildOnHandler(useState, "checked", useStateArgs);
+export const useDeferredOnChecked = buildOnHandler(
+  useDeferred,
+  "checked",
+  useDeferredArgs
+);
