02ad9288d77e84fade015e4d01af78af947c168e
Josh Gummersall <josh@standardlabs.dev>
2019-10-29 18:14:45 -0700

    react-hooks

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React, { useCallback, useState } from "react";
 
 const identity = a => a;
 
-export const useInput = (initialState, transform = identity) => {
+export const useOnChange = (initialState, transform = identity) => {
   const [value, setValue] = useState(initialState);
   const onChange = useCallback(
     ({ target: { value } }) => setValue(transform(value)),
@@ -12,12 +12,12 @@ export const useInput = (initialState, transform = identity) => {
   return [value, onChange, setValue];
 };
 
-export const useCheckbox = initialState => {
+export const useOnChecked = initialState => {
   const [checked, setChecked] = useState(initialState);
-  const onChange = useCallback(
+  const onChecked = useCallback(
     ({ target: { checked } }) => setChecked(checked),
     [setChecked]
   );
 
-  return [checked, onChange, setChecked];
+  return [checked, onChecked, setChecked];
 };
